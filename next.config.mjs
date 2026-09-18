f83549ede8247077a98e958beb91fb64c68ae7aa
oznuryemek2-ui <oznuryemek2@gmail.com>
2026-09-19 02:54:10 +0300

    Allow high-resolution catering imagery from Pexels

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,15 @@
 const nextConfig = {
   poweredByHeader: false,
   compress: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        pathname: "/photos/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
