501f1272a14d657c5d7cb3d5e9b8974876df10d5
oznuryemek2-ui <oznuryemek2@gmail.com>
2026-09-19 00:02:30 +0300

    Load redesigned visual system site-wide

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./redesign.css";
 import { Footer, Header } from "./components/SiteChrome";
 
 export const metadata:Metadata={
